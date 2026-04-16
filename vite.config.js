d3d428d3672cd6b566f2cb495e764d01983726de
Manuel-cyber247 <ufoarohchinecherem@gmail.com>
2026-04-16 22:21:33 +0100

    Fix MIME type issue for JavaScript files

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
-      input: {
-        main: './index.html'
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       }
     }
-  },
-  server: {
-    port: 3000
   }
 })
